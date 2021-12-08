985623d67c1d77dee7e6102a56f7aa1732f1afe3
anilkamatham <75293771+anilkamatham@users.noreply.github.com>
2021-12-08 14:25:08 +0530

    Add files via upload

---
+++ src/main/java/com/bookmymovie/movie/cloud/gateway/BookMyMovieCloudGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.bookmymovie.movie.cloud.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookMyMovieCloudGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookMyMovieCloudGatewayApplication.class, args);
+	}
+
+}
